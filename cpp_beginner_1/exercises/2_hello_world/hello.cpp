37e6257a00f6fc6263a9c5b3a45bb5db72f7d1ff
Jonas Friesslich <25103129+Fugashu@users.noreply.github.com>
2026-01-01 14:09:27 +0100

    init

---
+++ cpp_beginner_1/exercises/2_hello_world/hello.cpp
@@ -0,0 +1,20 @@
+/*
+Author: Jonas Friesslich
+Date: 01.01.2026
+Function: Simple Hello World
+*/
+
+// Single line comment
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Jonas lent C++!";
+    cout << "und freut sich auf den Kurs";
+
+
+    return 0;
+}
